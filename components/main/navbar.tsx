a5c29d5b558843d2c5d3f2a13d10240a7e251c66
Nakamura <153386228+NakamuraKuhiniko@users.noreply.github.com>
2024-02-21 23:43:36 +0900

    fix navbar

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -7,10 +7,7 @@ export const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001427] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <Link
-          href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
-        >
+        <Link href="#about-me" className="h-auto w-auto flex flex-row items-center">
           <Image
             src="/logo.png"
             alt="Logo"
@@ -50,9 +47,7 @@ export const Navbar = () => {
 
         <div className="flex flex-row gap-5">
           {SOCIALS.map(({ link, name, icon: Icon }) => (
-            <Link
-              href='#contact'
-            >
+            <Link href={link} key={name}>
               <Icon className="h-6 w-6 text-white" />
             </Link>
           ))}
